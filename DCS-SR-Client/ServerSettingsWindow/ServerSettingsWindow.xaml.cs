f815799c1aca2d42dc7086dba64c5205c961e267
Ciaran <cfisher88@gmail.com>
2016-08-17 08:36:43 +0100

    Implemented Server Settings Window

--- DCS-SR-Client/ServerSettingsWindow/ServerSettingsWindow.xaml.cs
+++ DCS-SR-Client/ServerSettingsWindow/ServerSettingsWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Ciribob.DCS.SimpleRadio.Standalone.Server;
 using MahApps.Metro.Controls;
 
 namespace Ciribob.DCS.SimpleRadio.Standalone.Client.UI
@@ -23,6 +24,21 @@ namespace Ciribob.DCS.SimpleRadio.Standalone.Client.UI
         public ServerSettingsWindow()
         {
             InitializeComponent();
+
+            var settings = ClientSync.ServerSettings;
+
+            SpectatorAudio.Content = settings[(int) ServerSettingType.SPECTATORS_AUDIO_DISABLED] == "DISABLED" ? "DISABLED" : "ENABLED";
+
+            CoalitionSecurity.Content = settings[(int)ServerSettingType.COALITION_AUDIO_SECURITY] == "ON" ? "ON" : "OFF" ;
+
+            LineOfSight.Content = settings[(int) ServerSettingType.LOS_ENABLED] == "ON" ? "ON" : "OFF";
+
+            Distance.Content = settings[(int)ServerSettingType.DISTANCE_ENABLED] == "ON" ? "ON" : "OFF";
+        }
+
+        private void CloseButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }

764b21eab94744fe867adb9b786371a6d1b3af65
Drofseh <Drofseh@users.noreply.github.com>
2023-04-18 01:17:05 -0700

    Add extended AN/ARC-186(V)FM to F-16

--- Scripts/DCS-SRS/Scripts/DCS-SimpleRadioStandalone.lua
+++ Scripts/DCS-SRS/Scripts/DCS-SimpleRadioStandalone.lua
@@ -3152,6 +3152,17 @@ function SR.exportRadioF16C(_data)
         _data.iff.mode4 = false
     end
 
+    -- Expansion Radio - Server Side Controlled
+    _data.radios[4].name = "AN/ARC-186(V)FM"
+    _data.radios[4].freq = 30.0 * 1000000 --VHF/FM opera entre 30.000 y 76.000 MHz.
+    _data.radios[4].modulation = 1
+    _data.radios[4].volume = 1.0
+    _data.radios[4].freqMin = 30 * 1000000
+    _data.radios[4].freqMax = 76 * 1000000
+    _data.radios[4].volMode = 1
+    _data.radios[4].freqMode = 1
+    _data.radios[4].expansion = true
+
     -- SR.log("IFF STATUS"..SR.JSON:encode(_data.iff).."\n\n")
 
     return _data

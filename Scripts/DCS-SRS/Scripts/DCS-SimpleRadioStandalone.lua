0d609d596b5c4814dc68569e0d658328bc97b0b9
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-08-15 20:04:58 -0400

    Waffling about what values to use. Made the CA roles 2000 series.
    10002001 is the start of CA roles so that intercom Offsets dont break the Unit id's

--- Scripts/DCS-SRS/Scripts/DCS-SimpleRadioStandalone.lua
+++ Scripts/DCS-SRS/Scripts/DCS-SimpleRadioStandalone.lua
@@ -302,16 +302,16 @@ function SR.exporter()
     
         if SR.lastKnownUnitType == 'artillery_commander' then
             _update.unit = "Artillery Command"
-            _update.unitId = (SR.lastKnownCoalition .. 100001002)
+            _update.unitId = (SR.lastKnownCoalition .. 100002001)
         elseif SR.lastKnownUnitType == 'forward_observer' then
             _update.unit = "Forward Observer"
-            _update.unitId = (SR.lastKnownCoalition .. 100001002)
+            _update.unitId = (SR.lastKnownCoalition .. 100002001)
         elseif SR.lastKnownUnitType == 'instructor' then
             _update.unit = "Instructor" -- Game Master
-            _update.unitId = (SR.lastKnownCoalition .. 100001002)
+            _update.unitId = (SR.lastKnownCoalition .. 100002001)
         elseif SR.lastKnownUnitType == 'observer' then
             _update.unit = "Observer"
-            _update.unitId = (SR.lastKnownCoalition .. 100001002)
+            _update.unitId = (SR.lastKnownCoalition .. 100002001)
         else
             _update.unitID = (SR.lastKnownCoalition .. 100001001)
         end

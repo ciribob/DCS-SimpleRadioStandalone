051a82706fe0ecd252dfb4c7579117013559bb9a
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-03-16 00:11:37 -0400

    Decoding Server Settings

--- DCS-SR-Client/Settings/ServerSettingsModel.cs
+++ DCS-SR-Client/Settings/ServerSettingsModel.cs
@@ -78,4 +78,104 @@ public partial class ServerSettingsModel : ObservableObject
 		
 		return frequencies;
 	}
+	
+	public void Decode(Dictionary<string, string> encodedServerSettings)
+	{
+		foreach (KeyValuePair<string, string> kvp in encodedServerSettings)
+		{
+			switch (kvp.Key)
+			{
+				case "0": case "SERVER_PORT":
+					ServerPort = int.Parse(kvp.Value);
+					break;
+				case "1": case "COALITION_AUDIO_SECURITY": 
+					IsCoalitionAudioSeperated = bool.Parse(kvp.Value);
+					break; 
+				case "2": case "SPECTATORS_AUDIO_DISABLED":
+					IsSpectatorsAudioDisabled = bool.Parse(kvp.Value);
+					break;
+				case "3": case "CLIENT_EXPORT_ENABLED":
+					IsClientExportAllowed = bool.Parse(kvp.Value);
+					break;
+				case "4": case "LOS_ENABLED":
+					IsLineOfSightCheckingEnabled = bool.Parse(kvp.Value);
+					break;
+				case "5": case "DISTANCE_ENABLED": 
+					IsDistanceCheckingEnabled = bool.Parse(kvp.Value);
+					break;
+				case "6": case "IRL_RADIO_TX": 
+					IsRadioTxEffectsEnabled = bool.Parse(kvp.Value);
+					break;
+				case "7": case "IRL_RADIO_RX_INTERFERENCE": 
+					IsRadioRxInterferenceEnabled = bool.Parse(kvp.Value);
+					break;
+				case "8": case "IRL_RADIO_STATIC": 
+					IsRadioStaticEffectsEnabled = bool.Parse(kvp.Value);
+					break;
+				case "9": case "RADIO_EXPANSION": 
+					IsExpandedRadiosAllowed = bool.Parse(kvp.Value);
+					break;
+				case "10": case "EXTERNAL_AWACS_MODE": 
+					IsExternalModeAllowed = bool.Parse(kvp.Value);
+					break;
+				case "11": case "EXTERNAL_AWACS_MODE_BLUE_PASSWORD": 
+					ExternalModeBluePassword = kvp.Value;
+					break;
+				case "12": case "EXTERNAL_AWACS_MODE_RED_PASSWORD": 
+					ExternalModeRedPassword = kvp.Value;
+					break;
+				case "13": case "CLIENT_EXPORT_FILE_PATH": 
+					ClientExportFileName = kvp.Value;
+					break;
+				case "14": case "CHECK_FOR_BETA_UPDATES": 
+					CheckForBetaUpdates = bool.Parse(kvp.Value);
+					break;
+				case "15": case "ALLOW_RADIO_ENCRYPTION": 
+					IsRadioEncryptionAllowed = bool.Parse(kvp.Value);
+					break;
+				case "16": case "TEST_FREQUENCIES": 
+					TestFrequencies = kvp.Value;
+					break;
+				case "17": case "SHOW_TUNED_COUNT": 
+					IsShowTunedListenerCount = bool.Parse(kvp.Value);
+					break;
+				case "18": case "GLOBAL_LOBBY_FREQUENCIES": 
+					GlobalLobbyFrequencies = kvp.Value;
+					break;
+				case "19": case "SHOW_TRANSMITTER_NAME": 
+					IsShowTransmitterNameEnabled = bool.Parse(kvp.Value);
+					break;
+				case "20": case "LOTATC_EXPORT_ENABLED": 
+					IsLotAtcExportEnabled = bool.Parse(kvp.Value);
+					break;
+				case "21": case "LOTATC_EXPORT_PORT": 
+					LotAtcExportPort = int.Parse(kvp.Value);
+					break;
+				case "22": case "LOTATC_EXPORT_IP": 
+					LotAtcExportIp = kvp.Value;
+					break;
+				case "23": case "UPNP_ENABLED": 
+					IsUpnpEnabled = bool.Parse(kvp.Value);
+					break;
+				case "24": case "RETRANSMISSION_NODE_LIMIT": 
+					RetransmissionNodeLimit = int.Parse(kvp.Value);
+					break;
+				case "25": case "STRICT_RADIO_ENCRYPTION": 
+					IsStrictRadioEncryptionEnabled = bool.Parse(kvp.Value);
+					break;
+				case "26": case "TRANSMISSION_LOG_ENABLED": 
+					IsTransmissionLogEnabled = bool.Parse(kvp.Value);
+					break;
+				case "27": case "TRANSMISSION_LOG_RETENTION": 
+					TransmissionLogRetentionLimit = int.Parse(kvp.Value);
+					break;
+				case "28": case "RADIO_EFFECT_OVERRIDE": 
+					IsRadioEffectOverrideEnabled = bool.Parse(kvp.Value);
+					break;
+				case "29": case "SERVER_IP": 
+					ServerIp = kvp.Value;
+					break;
+			}
+		}
+	}
 }

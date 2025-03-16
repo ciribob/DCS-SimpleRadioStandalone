3d6ef41fe7812abeca9860aa123d45ed043b3aae
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-03-16 17:35:48 -0400

    Remove Unneeded Using

--- DCS-SR-Client/Settings/SrsSettingsService.cs
+++ DCS-SR-Client/Settings/SrsSettingsService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;

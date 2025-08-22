1f36f2334bb675a6dd97c9bdfe2cea800eebc095
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-08-22 17:00:11 -0400

    Extract ExportRadio functions to Module File

---
+++ Scripts/DCS-SRS/Scripts/DCS-SRS-Modules/A10A.lua
@@ -0,0 +1,74 @@
+﻿function exportRadioA10A(_data)
+    _data.radios[2].name = "AN/ARC-186(V)"
+    _data.radios[2].freq = 124.8 * 1000000 --116,00-151,975 MHz
+    _data.radios[2].modulation = 0
+    _data.radios[2].secFreq = 121.5 * 1000000
+    _data.radios[2].volume = 1.0
+    _data.radios[2].freqMin = 116 * 1000000
+    _data.radios[2].freqMax = 151.975 * 1000000
+    _data.radios[2].volMode = 1
+    _data.radios[2].freqMode = 1
+
+    _data.radios[3].name = "AN/ARC-164 UHF"
+    _data.radios[3].freq = 251.0 * 1000000 --225-399.975 MHZ
+    _data.radios[3].modulation = 0
+    _data.radios[3].secFreq = 243.0 * 1000000
+    _data.radios[3].volume = 1.0
+    _data.radios[3].freqMin = 225 * 1000000
+    _data.radios[3].freqMax = 399.975 * 1000000
+    _data.radios[3].volMode = 1
+    _data.radios[3].freqMode = 1
+
+    _data.radios[3].encKey = 1
+    _data.radios[3].encMode = 1 -- FC3 Gui Toggle + Gui Enc key setting
+
+    _data.radios[4].name = "AN/ARC-186(V) FM"
+    _data.radios[4].freq = 30.0 * 1000000 --VHF/FM opera entre 30.000 y 76.000 MHz.
+    _data.radios[4].modulation = 1
+    _data.radios[4].volume = 1.0
+    _data.radios[4].freqMin = 30 * 1000000
+    _data.radios[4].freqMax = 76 * 1000000
+    _data.radios[4].volMode = 1
+    _data.radios[4].freqMode = 1
+
+    _data.radios[4].encKey = 1
+    _data.radios[4].encMode = 1 -- FC3 Gui Toggle + Gui Enc key setting
+
+    _data.control = 0;
+    _data.selected = 1
+
+    if SR.getAmbientVolumeEngine() > 10 then
+        -- engine on
+
+        --  local _door = SR.getButtonPosition(181)
+
+        --  if _door > 0.15 then
+        _data.ambient = { vol = 0.3, abType = 'a10' }
+        -- else
+        --     _data.ambient = {vol = 0.2,  abType = 'a10' }
+        --  end
+    else
+        -- engine off
+        _data.ambient = { vol = 0, abType = 'a10' }
+    end
+
+    return _data
+end
+
+--for A10C2
+local _a10c2 = {}
+_a10c2.enc = false
+_a10c2.encKey = 1
+_a10c2.volume = 1
+_a10c2.lastVolPos = 0
+_a10c2.increaseVol = false
+_a10c2.decreaseVol = false
+_a10c2.enableVolumeControl = false
+
+local result = { }
+
+function result.register(SR)
+    SR.exporters["A-10A"] = exportRadioA10A
+end
+
+return result

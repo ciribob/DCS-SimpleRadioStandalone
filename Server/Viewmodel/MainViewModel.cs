dee97601a904dca45bbd897c0ce41c7456e7aea9
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-08-25 22:32:42 -0400

    Move MainViewModel

---
+++ Server/Viewmodel/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Ciribob.DCS.SimpleRadio.Standalone.Common.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Ciribob.DCS.SimpleRadio.Standalone.Server.viewmodel;
+
+public partial class MainViewModel : ObservableObject
+{
+	[ObservableProperty] private ServerSettingsModel _serverSettings;
+	
+	public MainViewModel()
+	{
+		_serverSettings = new ServerSettingsModel();
+	}
+}

e762f8c12943291bf6e68112a562afdfb02a3be5
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-08-25 22:54:24 -0400

    Run commands

--- Server/Viewmodel/MainViewModel.cs
+++ Server/Viewmodel/MainViewModel.cs
@@ -1,5 +1,8 @@
-﻿using Ciribob.DCS.SimpleRadio.Standalone.Common.Models;
+﻿using System.Diagnostics;
+using System.Windows.Input;
+using Ciribob.DCS.SimpleRadio.Standalone.Common.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Ciribob.DCS.SimpleRadio.Standalone.Server.viewmodel;
 
@@ -19,5 +22,27 @@ public partial class MainViewModel : ObservableObject
 	public MainViewModel()
 	{
 		_serverSettings = new ServerSettingsModel();
+		StartStopCommand = new RelayCommand(StartStopServer);
+	}
+
+	public ICommand StartStopCommand { get; }
+	public void StartStopServer()
+	{
+		switch (ServerRunning)
+		{
+			case RunningState.Stopped:
+				ServerRunning = RunningState.Starting;
+
+				ServerRunning = RunningState.Running;
+				break;
+			case RunningState.Running:
+				ServerRunning = RunningState.Stopping;
+				
+				ServerRunning = RunningState.Stopped;
+				break;
+			
+			default:
+				break;
+		}
 	}
 }

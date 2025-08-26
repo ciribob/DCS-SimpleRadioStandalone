3d468da35107ce9b1ca72f7c31d09c64a68f20bb
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-08-25 22:36:12 -0400

    Add Manipulation of Test and Lobby frequency lists

--- Server/UI/MainWindow.axaml.cs
+++ Server/UI/MainWindow.axaml.cs
@@ -1,11 +1,47 @@
+using System.Diagnostics;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Ciribob.DCS.SimpleRadio.Standalone.Server.viewmodel;
 
 namespace Ciribob.DCS.SimpleRadio.Standalone.Server.UI;
 
 public partial class MainWindow : Window
 {
-	public MainWindow()
+	private MainViewModel ViewModel { get; }
+	
+	public MainWindow(MainViewModel viewModelFromConstructor)
 	{
+		ViewModel = viewModelFromConstructor;
 		InitializeComponent();
+	}
+
+	private void TestFrequenciesAddButton_OnClick(object? sender, RoutedEventArgs e)
+	{
+		if (TestFrequencyTextBox.Text == null) return;
+		ViewModel.ServerSettings.TestFrequencies.Add(TestFrequencyTextBox.Text);
+		TestFrequencyTextBox.Text = string.Empty;
+	}
+
+	private void TestFrequenciesRemoveButton_OnClick(object? sender, RoutedEventArgs e)
+	{
+		if ((sender as Button)?.Tag is string toRemove)
+		{
+			ViewModel.ServerSettings.TestFrequencies.Remove(toRemove);
+		}
+	}
+
+	private void GlobalFrequenciesAddButton_OnClick(object? sender, RoutedEventArgs e)
+	{
+		if (GlobalFrequencyTextBox.Text == null) return;
+		ViewModel.ServerSettings.GlobalLobbyFrequencies.Add(TestFrequencyTextBox.Text);
+		GlobalFrequencyTextBox.Text = string.Empty;
+	}
+	
+	private void GlobalFrequenciesRemoveButton_OnClick(object? sender, RoutedEventArgs e)
+	{
+		if ((sender as Button)?.Tag is string toRemove)
+		{
+			ViewModel.ServerSettings.GlobalLobbyFrequencies.Remove(toRemove);
+		}
 	}
 }

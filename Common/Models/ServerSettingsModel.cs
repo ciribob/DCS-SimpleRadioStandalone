74dcc339dd04fe5e411976c14708f44afd5f9b98
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-09-06 15:46:18 -0400

    Change to use ObservableProperty

--- Common/Models/ServerSettingsModel.cs
+++ Common/Models/ServerSettingsModel.cs
@@ -2,61 +2,47 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Ciribob.DCS.SimpleRadio.Standalone.Common.Models;
 
-public class ServerSettingsModel : INotifyPropertyChanged
+public partial class ServerSettingsModel : ObservableObject
 {
-	public bool IsLineOfSightEnabled {get; set;}
-	public bool IsDistanceLimitEnabled {get; set;}
-	public bool IsIRLRadioTXEffectsEnabled {get; set;}
-	public bool IsIRLRadioRXEffectsEnabled {get; set;}
-	public bool IsRadioExpansionAllowed {get; set;}
-	public bool IsRadioEffectOverrideOnGlobalEnabled { get; set; }
-	public int RetransmissionNodeLimit { get; set; }
 	
-	public bool IsCoalitionAudioSecurityEnabled {get; set;}
-	public bool IsSpectatorAudioDisabled {get; set;}
+	[ObservableProperty] private bool _isLineOfSightEnabled = false;
+	[ObservableProperty] private bool _isDistanceLimitEnabled = false;
+	[ObservableProperty] private bool _isIRLRadioTXEffectsEnabled = false;
+	[ObservableProperty] private bool _isIRLRadioRXEffectsEnabled = false;
+	[ObservableProperty] private bool _isRadioExpansionAllowed = false;
+	[ObservableProperty] private bool _isRadioEffectOverrideOnGlobalEnabled = false;
+	[ObservableProperty] private int _retransmissionNodeLimit = 0;
+	
+	[ObservableProperty] private bool _isCoalitionAudioSecurityEnabled = false;
+	[ObservableProperty] private bool _isSpectatorAudioDisabled = false;
 
 	public ObservableCollection<string> TestFrequencies { get; set; } = new() { "125.2", "142.5" };
 
-	public int ServerPort { get; set; }
-	public bool IsClientExportEnabled {get; set;}
-	public bool IsExternalModeEnabled {get; set;}
-	public string ExternalModeBlue { get; set; }
-	public string ExternalModeRed { get; set; }
-	public string ClientExportFilePath { get; set; }
-	public bool IsCheckForBetaUpdatesEnabled { get; set; }
-	public bool IsRadioEncryptionAllowed { get; set; }
-	public bool IsShowTunedCountEnabled { get; set; }
-	public ObservableCollection<string> GlobalLobbyFrequencies { get; set; } = new();
-	public bool IsShowTransmitterNameEnabled { get; set; }
-	public bool IsLotAtcExportEnabled { get; set; }
-	public int LotAtcExportPort { get; set; }
-	public string LotAtcExportIP { get; set; }
-	public bool isUPNPEnabled { get; set; }
-	public bool IsStrictRadioEncryptionEnabled { get; set; }
-	public bool IsTransmissionLogEnabled { get; set; }
-	public int TransmissionLogRetentionLimit { get; set; }
-	public string ServerIP { get; set; }
-	public bool IsServerPresetsEnabled { get; set; }
-	public string ServerPresetsPath { get; set; }
-	public bool IsHTTPServerEnabled { get; set; }
-	public int HTTPServerPort { get; set; }
-	
-	
-	public event PropertyChangedEventHandler PropertyChanged;
-
-	protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-	{
-		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-	}
-
-	protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
-	{
-		if (EqualityComparer<T>.Default.Equals(field, value)) return false;
-		field = value;
-		OnPropertyChanged(propertyName);
-		return true;
-	}
+	[ObservableProperty] private int _serverPort = 5002;
+	[ObservableProperty] private bool _isClientExportEnabled = false;
+	public bool IsExternalModeEnabled {get; set;} = false;
+	public string ExternalModePassBlue { get; set; } = string.Empty;
+	public string ExternalModePassRed { get; set; } = string.Empty;
+	public string ClientExportFilePath { get; set; } = string.Empty;
+	public bool IsCheckForBetaUpdatesEnabled { get; set; } = false;
+	public bool IsRadioEncryptionAllowed { get; set; } = true;
+	public bool IsShowTunedCountEnabled { get; set; } = true;
+	public ObservableCollection<string> GlobalLobbyFrequencies { get; set; } = new(){ "248.22" };
+	public bool IsShowTransmitterNameEnabled { get; set; } = false;
+	public bool IsLotAtcExportEnabled { get; set; } = false;
+	public int LotAtcExportPort { get; set; } = 10712;
+	public string LotAtcExportIP { get; set; } = "127.0.0.1";
+	public bool isUPNPEnabled { get; set; } = true;
+	public bool IsStrictRadioEncryptionEnabled { get; set; } = false;
+	public bool IsTransmissionLogEnabled { get; set; } = false;
+	public int TransmissionLogRetentionLimit { get; set; } = 2;
+	public string ServerIP { get; set; } = "0.0.0.0";
+	public bool IsServerPresetsEnabled { get; set; } = false;
+	public string ServerPresetsPath { get; set; } = string.Empty;
+	public bool IsHTTPServerEnabled { get; set; } = false;
+	public int HTTPServerPort { get; set; } = 8080;
 }

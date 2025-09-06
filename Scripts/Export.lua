f79990d09ab4cbf7bb4b994cf2f4c9511b479c37
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-09-06 15:57:43 -0400

    Revert formatting changes

--- Scripts/Export.lua
+++ Scripts/Export.lua
@@ -1,4 +1,2 @@
-pcall(function()
-    local dcsSr = require("lfs")
-    dofile(dcsSr.writedir() .. [[Mods\Services\DCS-SRS\scripts\DCS-SimpleRadioStandalone.lua]])
-end, nil)
+
+pcall(function() local dcsSr = require("lfs"); dofile(dcsSr.writedir() .. [[Mods\Services\DCS-SRS\scripts\DCS-SimpleRadioStandalone.lua]]); end, nil)

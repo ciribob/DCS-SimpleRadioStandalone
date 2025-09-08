b566a253c13d6262b7ce7e8a46b492605ae7be2c
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-09-08 18:35:37 -0400

    Add better design time tooling

---
+++ Server/Viewmodel/DesignMainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Caliburn.Micro;
+using Ciribob.DCS.SimpleRadio.Standalone.Common.Settings;
+using Ciribob.DCS.SimpleRadio.Standalone.Server.Model;
+
+namespace Ciribob.DCS.SimpleRadio.Standalone.Server.viewmodel;
+
+public class DesignMainViewModel(IEventAggregator eventAggregator, ServerSettingsModel serverSettingsModel)
+	: MainViewModel(eventAggregator, serverSettingsModel)
+{
+	
+	/// <summary>
+	/// Design time constructor.
+	/// </summary>
+	public DesignMainViewModel() : this(new EventAggregator(), new ServerSettingsModel(new EventAggregator(), new ServerSettingsStore()))
+	{
+	}
+}

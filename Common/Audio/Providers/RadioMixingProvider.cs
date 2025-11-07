9134a7bb3192c41958a5601dcb968bb3451ce149
gcask <53709079+gcask@users.noreply.github.com>
2025-11-07 08:40:20 -0500

    Ensure sources access is always under lock

--- Common/Audio/Providers/RadioMixingProvider.cs
+++ Common/Audio/Providers/RadioMixingProvider.cs
@@ -56,11 +56,6 @@ public class RadioMixingProvider : ISampleProvider
     }
 
     /// <summary>
-    ///     Returns the mixer inputs (read-only - use AddMixerInput to add an input
-    /// </summary>
-    public IEnumerable<ClientAudioProvider> MixerInputs => sources;
-
-    /// <summary>
     ///     The output WaveFormat of this sample provider
     /// </summary>
     public WaveFormat WaveFormat { get; }
@@ -91,13 +86,14 @@ public class RadioMixingProvider : ISampleProvider
         // Are we starved? Rehydrate.
         if (monoOffset < monoBufferLength)
         {
-            List<TransmissionSegment> segments = new(sources.Count);
+            List<TransmissionSegment> segments = null; 
 
             // Update sources by queueing incoming audio.
             var ky58Tone = false;
             var longestSegmentLength = 0;
             lock (sources)
             {
+                segments = new(sources.Count);
                 var index = sources.Count - 1;
                 var desired = monoBufferLength - monoOffset;
                 while (index >= 0)

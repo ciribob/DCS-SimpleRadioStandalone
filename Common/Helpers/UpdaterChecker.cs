736c6717f6ec2998ee7290143d723d9d3ed9e888
gcask <53709079+gcask@users.noreply.github.com>
2026-03-24 22:03:24 -0400

    Fix async/await for updater check.

--- Common/Helpers/UpdaterChecker.cs
+++ Common/Helpers/UpdaterChecker.cs
@@ -200,9 +200,12 @@ public class UpdaterChecker
         }
 
 
-        Task.Run(() =>
+        Task.Run(async () =>
         {
-            while (IsDCSRunning()) Thread.Sleep(5000);
+            while (IsDCSRunning())
+            {
+                await Task.Delay(TimeSpan.FromSeconds(5));
+            }
 
 #pragma warning disable CA1416
             var principal = new WindowsPrincipal(WindowsIdentity.GetCurrent());

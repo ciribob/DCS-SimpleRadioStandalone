767540c1a6fc43a95786468307c3be10d787e29d
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2026-06-24 19:51:44 -0400

    Add CLI package to Flake File

--- flake.nix
+++ flake.nix
@@ -7,6 +7,9 @@
 	};
 
 	outputs = inputs @ { self, ... }:
+	let
+		revision = "2.3.8.2"; # automatic sould be better, something like: self.shortRev or self.dirtyShortRev or "unknown";
+	in
 	inputs.flake-parts.lib.mkFlake {inherit inputs;} 
 	{
 		systems = [
@@ -14,14 +17,45 @@
 			"x86_64-linux"
 		];
 
-		perSystem = { pkgs, ... }:{
+		perSystem = { pkgs, lib, ... }:{
 			devShells = {
 				default = pkgs.mkShell {
-					nativeBuildInputs = with pkgs;[
-						dotnetCorePackages.sdk_10_0
-					];
+					nativeBuildInputs = with pkgs;[ dotnetCorePackages.sdk_10_0 stylua ];
 
 					DOTNET_BIN = "${pkgs.dotnetCorePackages.sdk_10_0}/bin/dotnet";
+				};
+			};
+
+		packages =
+			let
+				common-meta = with lib;{
+					homepage = "https://github.com/ciribob/DCS-SimpleRadioStandalone/";
+					license = with lib.licenses; [ gpl3Plus ];
+				};
+			in
+			{
+				server-cli = pkgs.buildDotnetModule {
+					pname ="DCS-SR-Server-CLI";
+					version = revision;
+
+					src = lib.fileset.toSource {
+						root = ./.;
+						fileset = lib.fileset.unions [
+							./Common
+							./SharedAudio
+							./ServerCommandLine
+						];
+					};
+
+					projectFile = "./ServerCommandLine/ServerCommandLine.csproj";
+					nugetDeps = ./ServerCommandLine/deps.json;
+				
+					dotnet-sdk = pkgs.dotnetCorePackages.sdk_10_0;
+					dotnet-runtime = pkgs.dotnetCorePackages.runtime_10_0;
+				 
+					meta = common-meta // {
+						description = "A platform agnostic CLI for running a Simple Radio Standalone Server.";
+					};
 				};
 			};
 		};

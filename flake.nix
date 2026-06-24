41749be8007194c6900aeed9e49998600daf0ecf
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2026-06-24 18:13:05 -0400

    Add Flake.nix

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+	 description = "A Simple Radio Standalone Flake";
+
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+		flake-parts.url = "github:hercules-ci/flake-parts";
+	};
+
+	outputs = inputs @ { self, ... }:
+	inputs.flake-parts.lib.mkFlake {inherit inputs;} 
+	{
+		systems = [
+			"x86_64-windows"
+			"x86_64-linux"
+		];
+
+		perSystem = { pkgs, ... }:{
+			devShells = {
+				default = pkgs.mkShell {
+					nativeBuildInputs = with pkgs;[
+						dotnetCorePackages.sdk_10_0
+					];
+
+					DOTNET_BIN = "${pkgs.dotnetCorePackages.sdk_10_0}/bin/dotnet";
+				};
+			};
+		};
+	};
+}
